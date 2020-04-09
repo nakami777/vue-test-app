59ca7c09c27a27935e4ada207b5d0d4a68c63aa2
NakanoNamiki <namiki.nakano.vertex@gmail.com>
2020-04-09 14:50:46 +0900

    第一

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,15 @@
+/* eslint-disable */
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <board />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import Board from "./components/Board.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: { Board }
+};
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
